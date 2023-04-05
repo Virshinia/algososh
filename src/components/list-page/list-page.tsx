130764c89501479e689f2a3926bfa38883e17d97
Virshinia <n.melnikova.spb@gmail.com>
2023-04-05 09:32:58 -0700

    feat: list-page css changes

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,6 +1,6 @@
 import React, {ReactElement, useMemo, useState} from "react";
 import {SolutionLayout} from "../ui/solution-layout/solution-layout";
-import styles from "../queue-page/queue-page.module.css";
+import styles from "./list-page.module.css";
 import {Input} from "../ui/input/input";
 import {Button} from "../ui/button/button";
 import {useForm} from "../../constants/hooks/useForm";
@@ -8,6 +8,7 @@ import {Circle} from "../ui/circle/circle";
 import {ElementStates} from "../../types/element-states";
 import {SHORT_DELAY_IN_MS} from "../../constants/delays";
 import {LinkedList, ILinkedItem} from "./linked-list";
+import {ArrowIcon} from "../ui/icons/arrow-icon";
 
 
 interface IStatus {
@@ -242,15 +243,19 @@ export const ListPage: React.FC = () => {
 
 
   const render = (arr: ILinkedItem[]) => {
-    return arr.map(({top, bottom, value}, index) =>
-        <Circle
+    return arr.map(({top, bottom, value, next}, index) =>
+        <>
+          <Circle
             key={index}
             index={index}
             head={ top ? top : index === 0 ? "head" : ""}
             tail={bottom ? bottom : index === list.length - 1 ? "tail" : ""}
             letter={value}
             state={handleCircleStateChange(index, bottom, top)}
-        />)
+          />
+          {next && <ArrowIcon/>}
+        </>
+    )
   }
 
   return (
@@ -262,7 +267,7 @@ export const ListPage: React.FC = () => {
             maxLength={4}
             isLimitText={true}
             name='value'
-            extraClass='mr-6'
+            extraClass={`mr-6 ${styles.input}`}
         />
         <Button
             text="Добавить в head"
@@ -270,7 +275,7 @@ export const ListPage: React.FC = () => {
             onClick={addToHead}
             isLoader={status.addHead}
             disabled={values.value === '' || Object.values(status).includes(true)}
-            extraClass='mr-6'
+            extraClass={`mr-6 ${styles.button}`}
 
         />
         <Button
@@ -279,7 +284,7 @@ export const ListPage: React.FC = () => {
             onClick={addToTail}
             isLoader={status.addTail}
             disabled={values.value === '' || Object.values(status).includes(true)}
-            extraClass='mr-6'
+            extraClass={`mr-6 ${styles.button}`}
         />
         <Button
             text="Удалить из head"
@@ -287,7 +292,7 @@ export const ListPage: React.FC = () => {
             onClick={deleteFromHead}
             isLoader={status.removeHead}
             disabled={list.length < 1 || Object.values(status).includes(true)}
-            extraClass='mr-6'
+            extraClass={`mr-6 ${styles.button}`}
         />
         <Button
             text="Удалить из tail"
@@ -295,6 +300,7 @@ export const ListPage: React.FC = () => {
             onClick={deleteFromTail}
             isLoader={status.removeTail}
             disabled={list.length < 1 || Object.values(status).includes(true)}
+            extraClass={styles.button}
         />
       </form>
 
@@ -307,7 +313,7 @@ export const ListPage: React.FC = () => {
             onChange={handleChange}
             value={values.index}
             name='index'
-            extraClass='mr-6'
+            extraClass={`mr-6 ${styles.input}`}
         />
         <Button
             text="Добавить по индексу"
@@ -315,7 +321,7 @@ export const ListPage: React.FC = () => {
             onClick={insertAtIndex}
             isLoader={status.addByIndex}
             disabled={values.value === '' || values.index === null || Object.values(status).includes(true)}
-            extraClass='mr-6'
+            extraClass={`mr-6 ${styles.button}`}
 
         />
         <Button
@@ -324,6 +330,7 @@ export const ListPage: React.FC = () => {
             onClick={removeIndex}
             isLoader={status.removeByIndex}
             disabled={list.length - 1 < values.index || Object.values(status).includes(true)}
+            extraClass={styles.button}
         />
       </form>
       {list && <div className={styles.result}>{render(list)}</div>}
