1990bf86f6ac3e1c813591ab55842e97ef987886
Virshinia <n.melnikova.spb@gmail.com>
2023-04-19 15:35:07 -0700

    feat: e2e routing test

--- cypress/e2e/is-available.spec.cy.ts
+++ cypress/e2e/is-available.spec.cy.ts
@@ -1,7 +1,7 @@
 import {TEST_LOCAL_URL} from "../../src/constants/utils";
 
 describe('app is available', () => {
-  it('should be available on localhost:3000', () => {
+  it(`should be available on ${TEST_LOCAL_URL}`, () => {
     cy.visit(TEST_LOCAL_URL);
   })
 })
---
+++ cypress/e2e/routing.spec.cy.ts
@@ -0,0 +1,25 @@
+import {TEST_LOCAL_URL} from "../../src/constants/utils";
+
+function routing (text: string, url: string, title: string) {
+  it (text, () => {
+    cy.get(`a[href*='${url}']`).click()
+    cy.contains(title)
+    cy.get('button').contains('К оглавлению').click()
+  })
+}
+describe('app is available', () => {
+
+  before(() => {
+    cy.visit(TEST_LOCAL_URL);
+  });
+
+  routing('should open string page', 'recursion', 'Строка');
+  routing('should open fibonacci page', 'fibonacci', 'Последовательность Фибоначчи')
+  routing('should open sorting page', 'sorting', 'Сортировка массива')
+  routing('should open stack page', 'stack', 'Стек')
+  routing('should open queue page', 'queue', 'Очередь')
+  routing('should open linked-list page', 'list', 'Связный список')
+
+
+
+})
